6321c02c9df14dfc32e2389b85f0ecb8339433d2
davidhathaway <david.hathaway@capita.co.uk>
2012-06-21 14:26:00 +0100

    contact with smtp

--- DayDramaing/Controllers/HomeController.cs
+++ DayDramaing/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DayDramaing.Models;
+using DayDramaing.Common;
+using System.Net.Mail;
 
 namespace DayDramaing.Controllers
 {
@@ -26,5 +28,38 @@ namespace DayDramaing.Controllers
             return View(new ContactModel());
         }
 
+        [CaptchaValidatorAttribute]
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var from = model.Email;
+                    var to = "info@daydrama-ing.co.uk";
+                    var subject = "Contact Form:";
+                    var body = string.Format("Enquiry:{0}Name: {1}{0}Telephone: {2}{0}Details: {3}", Environment.NewLine, model.Name, model.Telephone, model.Enquiry);
+
+                    //send msg
+                    var smtpClient = new SmtpClient();
+                    var msg = new MailMessage(from, to, subject, body);
+                    smtpClient.Send(msg);
+                    return RedirectToAction("ContactSuccess");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("Error:", "There was an error sending your contact details.");
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult ContactSuccess()
+        {
+            return View();
+        }
     }
 }
